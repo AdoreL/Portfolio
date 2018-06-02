51a7d83daea3c02ef837c9e9a8cf5df9bd56811f
AdoreL <AdoreL@users.noreply.github.com>
2018-06-02 22:18:05 +0100

    Update template_oop.py

--- test/template_oop.py
+++ test/template_oop.py
@@ -15,7 +15,7 @@ app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
 app.config["MAIL_USERNAME"] = 'ninjalucario98@gmail.com'
-app.config["MAIL_PASSWORD"] = 'tokyomewmew98'
+app.config["MAIL_PASSWORD"] = ''
 
 mail.init_app(app)
 Bootstrap(app)
